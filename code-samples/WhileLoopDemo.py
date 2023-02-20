0f89b9d89364717e349787573b6d770132b2c5fe
Saurabhh <itssaurabh26@gmail.com>
2023-02-20 18:21:33 +0530

    loop demo python

---
+++ code-samples/WhileLoopDemo.py
@@ -0,0 +1,18 @@
+# While with break
+i = 1
+while i < 6:
+  print(i)
+  if i == 3:
+    break
+  i += 1
+
+
+
+# While with continue
+
+i = 0
+while i < 6:
+  i += 1
+  if i == 3:
+    continue
+  print(i)
